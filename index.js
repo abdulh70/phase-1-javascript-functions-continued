fdbad82fc5af8e7449cdb0a461556b100f4e3077
abdulh70 <abdulazizh70@gmail.com>
2024-10-09 17:03:52 +0300

    added functions conti

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(act = "roller-skate") {
+    return `This Saturday, I want to ${act}!`;
+  }
+  const mondayWork = function(x = 'go to the office'){
+    return `This Monday, I will ${x}.`
+}
+function wrapAdjective(visual = '*'){
+    return function(y = "special"){
+        return `You are ${visual}${y}${visual}!`
+    }
+}
